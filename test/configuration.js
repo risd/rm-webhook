af5c01bf6914e95a41da1430d478c22fab06c64e
rubonics <rubenrodriguez+github@gmail.com>
2022-11-21 14:46:26 -0400

    updates for async await wh lib

--- test/configuration.js
+++ test/configuration.js
@@ -1,9 +1,9 @@
 const test = require( 'tape' )
 
-test( 'configuration', function ( t ) {
+test( 'configuration', async function ( t ) {
   t.plan( 1 )
 
-  const conf = require( '../lib/configuration.js' )()
+  const conf = await require( '../lib/configuration.js' )()
 
   t.assert( typeof conf === 'object', 'successfully loaded configuration' )
 } )
